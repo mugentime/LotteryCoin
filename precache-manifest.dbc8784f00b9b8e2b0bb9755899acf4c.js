54a2fdeddf8c2e0ccc32d92d91cbf3cb51abf564
mugentime <je2al@hotmail.com>
2019-05-02 09:43:46 -0400

    Updates

---
+++ precache-manifest.dbc8784f00b9b8e2b0bb9755899acf4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LotteryCoin/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "44be3e267d1e41031de5",
+    "url": "/LotteryCoin/static/js/runtime~main.3a28495f.js"
+  },
+  {
+    "revision": "39d26b03bc8639d3185b",
+    "url": "/LotteryCoin/static/js/main.473ec38d.chunk.js"
+  },
+  {
+    "revision": "cf39c8bdae35fd3ea997",
+    "url": "/LotteryCoin/static/js/2.16cadc22.chunk.js"
+  },
+  {
+    "revision": "39d26b03bc8639d3185b",
+    "url": "/LotteryCoin/static/css/main.e3d9634d.chunk.css"
+  },
+  {
+    "revision": "cef2ec8a441d3d68b8d6219a633f41e8",
+    "url": "/LotteryCoin/index.html"
+  }
+];
