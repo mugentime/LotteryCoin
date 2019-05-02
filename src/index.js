f1534694b0ab7342c928a8fc91c87e4a6fa702fc
mugentime <je2al@hotmail.com>
2019-05-02 09:31:39 -0400

    'initial'

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Web3Provider } from 'react-web3';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
